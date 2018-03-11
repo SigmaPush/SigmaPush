a334fa7baf05ce86a3ddf80451884440f42947a7
ping1z <zhongyp.design@gmail.com>
2018-03-11 14:11:48 -0500

    update styles

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from './reducers';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './assets.js';
+
 import Search from './components/search';
 import Article from './components/article';
 import Author from './components/author';
@@ -13,21 +13,23 @@ import Reader from './components/reader';
 import Home from './components/home';
 import registerServiceWorker from './registerServiceWorker';
 
+import reducers from './reducers';
+
 let store = createStore(reducers)
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={ store }>
     <BrowserRouter >
       <div>
         <Switch>
-          <Route path="/search/" component={Search} />
-          <Route path="/Article/:id" component={Article} />
-          <Route path="/Author/:id" component={Author} />
-          <Route path="/Reader/:id" component={Reader} />
-          <Route path="/" component={Home} />
+          <Route path="/search/" component={ Search } />
+          <Route path="/Article/:id" component={ Article } />
+          <Route path="/Author/:id" component={ Author } />
+          <Route path="/Reader/:id" component={ Reader } />
+          <Route path="/" component={ Home } />
         </Switch>
       </div>
-    </BrowserRouter >
+    </BrowserRouter>
   </Provider>
   , document.getElementById('root'));
 
