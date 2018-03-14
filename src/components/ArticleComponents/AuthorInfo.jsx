0ed79b6bbc6d607d1b12d5ca782f6eff0693c7f3
SNCDefenser <snczhangyujie@gmail.com>
2018-03-13 22:04:25 -0500

    1

--- src/components/ArticleComponents/AuthorInfo.jsx
+++ src/components/ArticleComponents/AuthorInfo.jsx
@@ -8,7 +8,7 @@ export default class AuthorInfo extends Component {
         <div className="card-body">
           <h5 className="card-title">Author name: 小粉红</h5>
           <p className="card-text">My name is Yiping Zhong, I am also a little pink. Now I am here waiting for you. memeda</p>
-          <a href="" class="btn btn-primary">Go Author</a>
+          <a href="" className="btn btn-primary">Go Author</a>
         </div>
       </div>
     );
