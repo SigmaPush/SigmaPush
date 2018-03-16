61ca4d67ec51a4761c52b33b16ad358b530b5551
ping1z <zhongyp.design@gmail.com>
2018-03-16 00:23:55 -0500

    temporary commit

--- src/components/Home/AuthorList.jsx
+++ src/components/Home/AuthorList.jsx
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 class AuthorList extends Component {
   render() {
     return (
-      <div>
-        AuthorList
+      <div class="list-group">
+        <div class="row">
+          <div class="col-md-3">name</div>
+          <div class="col-md-9">data</div>
+        </div>
       </div>
     );
   }
