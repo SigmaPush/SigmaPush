47e164bd94d8773cb18c3b73d0f588229a7cf384
lidaxia2017 <mr.liwei1991@gmail.com>
2018-03-18 18:00:41 -0500

    update JS styles of actions and reducers

--- src/actions/action_tag.js
+++ src/actions/action_tag.js
@@ -1,12 +1,33 @@
 const dataArray = [
-  { id: "tag1", name: "face recognition" },
-  { id: "tag2", name: "python" },
-  { id: "tag3", name: "tensorflow" },
-  { id: "tag4", name: "java" },
-  { id: "tag5", name: "dynamic programming" },
-  { id: "tag6", name: "multi-threading" },
-  { id: "tag7", name: "MySQL" },
-]
+  {
+    id: 'tag1',
+    name: 'face recognition',
+  },
+  {
+    id: 'tag2',
+    name: 'python',
+  },
+  {
+    id: 'tag3',
+    name: 'tensorflow',
+  },
+  {
+    id: 'tag4',
+    name: 'java',
+  },
+  {
+    id: 'tag5',
+    name: 'dynamic programming'
+  },
+  {
+    id: 'tag6',
+    name: 'multi-threading'
+  },
+  {
+    id: 'tag7',
+    name: 'MySQL',
+  },
+];
 
 export const LIST_TAG = 'list_tag';
 
@@ -14,5 +35,5 @@ export function listTag() {
   return {
     type: LIST_TAG,
     payload: dataArray,
-  }
-}
+  };
+};
--- src/reducers/reducer_article.js
+++ src/reducers/reducer_article.js
@@ -10,4 +10,4 @@ export default function (state = {}, action) {
     default:
       return state;
   }
-}
+};
