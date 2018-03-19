9a816b747324b09f9e86d8d999f833f63615a178
lidaxia2017 <mr.liwei1991@gmail.com>
2018-03-18 20:25:26 -0500

    update JS styles of actions and reducers (#17)

--- src/actions/action_catalog.js
+++ src/actions/action_catalog.js
@@ -1,9 +1,21 @@
 const dataArray = [
-  { id: "cat1", name: "Deep Learning" },
-  { id: "cat2", name: "Algorithm" },
-  { id: "cat3", name: "Database" },
-  { id: "cat4", name: "Network" }
-]
+  {
+    id: 'cat1',
+    name: 'Deep Learning',
+  },
+  {
+    id: 'cat2',
+    name: 'Algorithm',
+  },
+  {
+    id: 'cat3',
+    name: 'Database',
+  },
+  {
+    id: 'cat4',
+    name: 'Network',
+  },
+];
 
 export const LIST_CATALOG = 'list_catalog';
 
@@ -11,5 +23,5 @@ export function listCatalog() {
   return {
     type: LIST_CATALOG,
     payload: dataArray,
-  }
-}
+  };
+};
--- src/reducers/reducer_author.js
+++ src/reducers/reducer_author.js
@@ -2,12 +2,12 @@ import _ from 'lodash';
 import { GET_AUTHOR, LIST_AUTHOR } from "../actions/action_author";
 
 export default function (state = {}, action) {
-    switch (action.type) {
-        case GET_AUTHOR:
-            return { ...state, [action.payload.id]: action.payload };
-        case LIST_AUTHOR:
-            return _.mapKeys(action.payload, 'id');
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case GET_AUTHOR:
+      return { ...state, [action.payload.id]: action.payload };
+    case LIST_AUTHOR:
+      return _.mapKeys(action.payload, 'id');
+    default:
+      return state;
+  }
+};
