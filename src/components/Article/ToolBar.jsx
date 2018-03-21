969ad8391387eaf670281ac6240d30660a12095b
SNCDefenser <snczhangyujie@gmail.com>
2018-03-20 23:57:50 -0500

    Article with comment (#24)
    * add toobar at the bottem of the article, add the input area for comment, adjust the layout of tags
    
    * fit some little bug  change class to className

--- src/components/Article/ToolBar.jsx
+++ src/components/Article/ToolBar.jsx
@@ -4,16 +4,16 @@ export default class ToolBar extends Component {
   render() {
     return (
       <div className="btn-group-vertical">
-        <button type="button" className="btn btn-secondary">
+        <button type="button" className="btn btn-primary">
           Like
         </button>
-        <button type="button" className="btn btn-secondary">
+        <button type="button" className="btn btn-primary">
           Share
         </button>
-        <button type="button" className="btn btn-secondary">
+        <button type="button" className="btn btn-primary">
           Fav
         </button>
-        <button type="button" className="btn btn-secondary">
+        <button type="button" className="btn btn-primary">
           Comm
         </button>
       </div>
